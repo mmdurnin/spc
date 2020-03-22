8b27ae12e4e70de5e6ab9f41f835cd46896afa45
Maureen Durnin <maureenmdurnin@gmail.com>
2020-03-22 16:20:44 -0700

    clean up code on banner

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import bannerVid from "../../images/background_3.mp4";
-import photo1 from '../../images/headstand_1.jpg'
-import photo2 from '../../images/headstand_2_edited.jpg'
-import photo3 from '../../images/ph_1_edited.jpg'
-import photo4 from '../../images/photo_bw.jpg'
 import '../../stylesheets/banner.css';
 
 class Banner extends React.Component{
@@ -14,31 +10,13 @@ class Banner extends React.Component{
   render() {
     return (
       <div className="banner-container row">
-        {/* <section className="banner-left">
-          <h1>
-            STYLE <br /> PROZ <br /> CREW
-          </h1>
-        </section>
-        <div className="banner-right row">
-          <section className="row">
-            <img src={photo1} alt="" />
-            <div className="column collage">
-              <img src={photo2} alt="" />
-              <img src={photo3} alt="" />
-            </div>
-          </section>
-          <video id="background-video" loop autoPlay muted>
-            <source src={bannerVid} type="video/mp4" />
-          </video> */}
-          <div className="gradient-bw">
-            <h1>STYLE PROZ CREW</h1>
-          </div>
-          <video id="background-video" autoPlay muted>
-            <source src={bannerVid} type="video/mp4" />
-          </video>
-          {/* <img src={photo4} alt=""/> */}
+         <div className="gradient-bw">
+          <h1>STYLE PROZ CREW</h1>
         </div>
-      // </div>
+        <video id="background-video" autoPlay muted>
+          <source src={bannerVid} type="video/mp4" />
+        </video>
+      </div>
     );
   }
 }
