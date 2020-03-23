90aa83f9aea60ca53ec28d1a53555bb67788058b
Maureen Durnin <maureenmdurnin@gmail.com>
2020-03-22 19:48:35 -0700

    wrap components in switch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import '../App.css';
-import Banner from './banner/banner';
 import NavBar from './nav-bar/nav';
-import About from './about/about';
+import Home from './home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Banner />
-      <About />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+import Banner from "./banner/banner";
+import About from "./about/about";
+
+function Home() {
+  return (
+    <div>
+      <Banner />
+      <About />
+    </div>
+  );
+}
+
+export default Home;
