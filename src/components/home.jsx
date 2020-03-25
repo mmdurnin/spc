cc7925681cb43c72ee612d10a0816effc0ae2c9f
Maureen Durnin <maureenmdurnin@gmail.com>
2020-03-25 15:26:20 -0700

    rename photo index to photo index home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../App.css";
 import Banner from "./banner/banner";
 import About from "./about/about";
-import PhotoIndex from "./media/photo-index"
+import PhotoIndex from "./media/photo-index-home"
 
 function Home() {
   return (
