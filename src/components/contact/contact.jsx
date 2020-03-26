2bbb998cd77bb73320bf35a5628a225454da35e4
Maureen Durnin <maureenmdurnin@gmail.com>
2020-03-25 19:43:27 -0700

    update contact form background with images and gradient

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import '../../stylesheets/contact-form.css';
+
+import photo1 from '../../images/ph_1_edited.jpg';
+import photo2 from '../../images/ph_5.jpg';
+import photo3 from '../../images/ph_18.jpeg';
+import photo4 from '../../images/headstand_2_edited.jpg';
+
+const imagesArray = [photo1, photo2, photo3, photo4]
 
 class ContactForm extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {photos: imagesArray}
   }
 
   render() {
     return(
-      <div>
-
+      <div className="contact-page">
+        <div className="background-photos column">
+          {
+            this.state.photos.map((photo, idx) => {
+              return <img src={photo} key={idx} />
+            })
+          }
+          <div />
+        </div>
       </div>
     )
   }
