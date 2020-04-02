cb01df4c66f1f295564213df953175e7f92e3e34
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-02 09:15:54 -0700

    change proxy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const nodemailer = require("nodemailer");
 const path = require('path');
 const cors = require("cors");
