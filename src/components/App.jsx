035762d00ad29794cbf5598234675f4109114209
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-04 16:32:17 -0700

    add files for auth route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,35 @@ import Calendar from './events/events';
 import Media from './media/photo-index-media';
 import ContactForm from './contact/contact';
 
+// export const AuthContext = React.createContext();
+// const getInitialState = {
+//   isAuthenticated: false,
+//   user: null,
+//   token: null
+// };
+// const reducer = (state, action) => {
+//   switch(action.type) {
+//     case "LOGIN":
+//       localStorage.setItem("user", JSON.stringify(action.payload.user));
+//       localStorage.setItem("token", JSON.stringify(action.payload.token));
+//       return {
+//         ...state,
+//         isAuthenticated: true,
+//         user: action.payload.user,
+//         token: action.payload.token
+//       };
+//     case "LOGOUT":
+//       localStorage.clear();
+//       return {
+//         ...state,
+//         isAuthenticated: false,
+//         user: null
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
 function App() {
   return (
     <div className="App">
