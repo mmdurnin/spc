4eaddaddebd4168b964f3847ba7921f11703ba27
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-08 12:32:12 -0700

    create login form and auth button

---
+++ src/components/edit/auth-button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AuthButton = (props) => {
+  return(
+    <button className="auth-button">{props.title}</button>
+  )
+}
+
+export default AuthButton;
--- src/components/edit/not-authenticated/login.jsx
+++ src/components/edit/not-authenticated/login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AuthButton from '../auth-button';
 
 class Login extends React.Component {
   constructor(props) {
@@ -8,7 +9,27 @@ class Login extends React.Component {
   render() {
     return(
       <div>
-        
+        <form>
+          <label htmlFor="username">
+            Username
+            <input 
+              type="text"
+              name="username"
+              id="username"
+            />
+          </label>
+
+          <label htmlFor="password">
+            Password
+            <input 
+              type="password"
+              name="password"
+              id="password"
+            />
+          </label>
+
+          <AuthButton title={"Admin Login"} />
+        </form>
       </div>
     )
   }
