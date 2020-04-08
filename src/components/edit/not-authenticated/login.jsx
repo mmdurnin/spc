3628ebc757bda3cb3252127368d3525eb25e5778
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-08 12:33:35 -0700

    change login form to hooks config

--- src/components/edit/not-authenticated/login.jsx
+++ src/components/edit/not-authenticated/login.jsx
@@ -1,38 +1,33 @@
 import React from 'react';
 import AuthButton from '../auth-button';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const Login = () => {
 
-  render() {
-    return(
-      <div>
-        <form>
-          <label htmlFor="username">
-            Username
-            <input 
-              type="text"
-              name="username"
-              id="username"
-            />
-          </label>
+  return(
+    <div>
+      <form>
+        <label htmlFor="username">
+          Username
+          <input 
+            type="text"
+            name="username"
+            id="username"
+          />
+        </label>
 
-          <label htmlFor="password">
-            Password
-            <input 
-              type="password"
-              name="password"
-              id="password"
-            />
-          </label>
+        <label htmlFor="password">
+          Password
+          <input 
+            type="password"
+            name="password"
+            id="password"
+          />
+        </label>
 
-          <AuthButton title={"Admin Login"} />
-        </form>
-      </div>
-    )
-  }
+        <AuthButton title={"Admin Login"} />
+      </form>
+    </div>
+  )
 }
 
 export default Login;
