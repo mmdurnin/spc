25e7c02bcbf809db3c293d82f5ecdd7089084051
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-09 12:37:44 -0700

    remove console logs and unused code

--- src/util/api-util.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-export const confirmSession = token => {
-  return $.ajax({
-    url: `http://localhost:3002/api/confirm-session?token=${token}`,
-    method: 'GET'
-  })
-}
