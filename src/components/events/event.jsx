c6bad39178840873eff66faab20e82e13712bdb0
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-10 20:12:58 -0700

    change event page styling

--- src/components/events/event.jsx
+++ src/components/events/event.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ScrollAnimation from "react-animate-on-scroll";
+// import { Link } from 'react-router-dom';
 
 const Event = (props) => {
 
   const image = (props.eventDetails.img_src) ? <img src={props.eventDetails.img_src} /> : null;
+  const button = (props.eventDetails.url && props.eventDetails.url_name) ? 
+    <a target="_blank" href={props.eventDetails.url}><button>{props.eventDetails.url_name}</button></a>
+    : null;
 
   return (
     <ScrollAnimation animateIn="zoomIn" duration={1}>
       <div className="event">
-        <div className="event-image-frame">{image}</div>
+        <h1>{props.eventDetails.event_name}</h1>
+        {/* <div className="event-image-frame">{image}</div> */}
+        {button}
       </div>
     </ScrollAnimation>
   );
