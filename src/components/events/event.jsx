08f77ce943d08cc06cee931496ae775ef7288058
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-11 14:44:32 -0700

    redo styling on event

--- src/components/events/event.jsx
+++ src/components/events/event.jsx
@@ -8,16 +8,36 @@ const Event = (props) => {
   const button = (props.eventDetails.url && props.eventDetails.url_name) ? 
     <a target="_blank" href={props.eventDetails.url}><button>{props.eventDetails.url_name}</button></a>
     : null;
+  const date = props.eventDetails.event_date.split(" ").map((word, idx) => {
+    return <h4 key={idx}>{word}</h4>
+  })
 
   return (
     <ScrollAnimation animateIn="zoomIn" duration={1}>
-      <div className="event column">
-        <h1>{props.eventDetails.event_name}</h1>
-        <div className="row">
-          <div className="event-image-frame">{image}</div>
-          {button}
+      <div className="event row">
+        <div className="event-image-frame">
+          {image}
+          <div className="row">
+            <h4>{props.eventDetails.event_date}</h4>
+            <h4>|</h4>
+            <h4>{props.eventDetails.venue_name.toUpperCase()}</h4>
+          </div>
+        </div>
+        <div className="column">
+          <h1>{props.eventDetails.event_name}</h1>
+          <p>{props.eventDetails.event_description}</p>
+          <div className="row"></div>
         </div>
       </div>
+      {/* <div className="event column"> */}
+      {/* <h1>{props.eventDetails.event_name}</h1>
+        <p>{props.eventDetails.event_description}</p> */}
+      {/* <section>{date}</section> */}
+      {/* <section>{props.eventDetails.event_date}</section> */}
+      {/* <div className="row"> */}
+      {/* {button} */}
+      {/* </div> */}
+      {/* </div> */}
     </ScrollAnimation>
   );
 }
