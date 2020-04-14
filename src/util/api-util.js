56f0ea2b05989f471a416eb03e9ea7d0687ad5cb
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-13 17:53:25 -0700

    create api util for mailist list routes

--- src/util/api-util.js
+++ src/util/api-util.js
@@ -33,3 +33,30 @@ export const removeEvent = (key) => {
     }
   })
 }
+
+export const fetchMailingList = () => {
+  return $.ajax({
+    url: "/api/admin/mailing_list",
+    method: 'GET'
+  });
+}
+
+export const addEmail = (record) => {
+  return $.ajax({
+    url: 'http://localhost:3002/api/email',
+    method: 'POST',
+    data: record
+  })
+}
+
+export const deleteEmail = (id) => {
+  const header = localStorage.token;
+  return $.ajax({
+    url: 'http://localhost:3002/api/admin/email',
+    method: 'DELETE',
+    data: id,
+    headers: {
+      token: header
+    }
+  })
+}
