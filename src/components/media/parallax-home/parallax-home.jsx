8a14f964f72985e949df554bcb5a41c24c2409e0
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-21 12:36:34 -0700

    style text overlay with exclusion mix blend mode

--- src/components/media/parallax-home/parallax-home.jsx
+++ src/components/media/parallax-home/parallax-home.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import image1 from '../../../images/parallax/ph_29_1.jpg';
-import image2 from '../../../images/parallax/ph_29__2.png';
-import image3 from '../../../images/parallax/ph_29_3.png';
-import image4 from '../../../images/parallax/ph_29_4.png';
+import Parallax from "react-rellax";
+// import image1 from '../../../images/parallax/ph_29_1.jpg';
+// import image2 from '../../../images/parallax/ph_29__2.png';
+// import image3 from '../../../images/parallax/ph_29_3.png';
+// import image4 from '../../../images/parallax/ph_29_4.png';
+import image from '../../../images/ph_29_vignette.jpg'
 import '../../../stylesheets/parallax.css';
 
 function ParallaxHome() {
-  return(
+  return (
     <div className="parallax-container">
-      <img src={image1} className="par par-1" />
-      <img src={image2} className="par par-2" />
-      <img src={image3} className="par par-3" />
-      <img src={image4} className="par par-4" />
-      <h1 className="par">The Crew</h1>
+      <Parallax speed={1}>
+        <img src={image} className="par par-1" />
+        <Parallax speed={-1.5} className="par par-text">The Crew</Parallax>
+      </Parallax>
     </div>
-  )
+  );
 }
 
 export default ParallaxHome;
