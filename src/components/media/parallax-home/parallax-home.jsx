9a341e9dec342666a5f301224ddbe7db3e6284ae
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-20 22:46:02 -0700

    add parallax images and creat component

---
+++ src/components/media/parallax-home/parallax-home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import image1 from '../../../images/parallax/ph_29_1.jpg';
+import image2 from '../../../images/parallax/ph_29_2.jpg';
+import image3 from '../../../images/parallax/ph_29_3.jpg';
+import image4 from '../../../images/parallax/ph_29_4.jpg';
