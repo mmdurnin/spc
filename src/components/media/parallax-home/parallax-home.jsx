bb367dac9c54626581f2c96e5b3f76853a6f8878
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-21 12:53:12 -0700

    update font color and weight on parallax text

--- src/components/media/parallax-home/parallax-home.jsx
+++ src/components/media/parallax-home/parallax-home.jsx
@@ -4,14 +4,16 @@ import Parallax from "react-rellax";
 // import image2 from '../../../images/parallax/ph_29__2.png';
 // import image3 from '../../../images/parallax/ph_29_3.png';
 // import image4 from '../../../images/parallax/ph_29_4.png';
-import image from '../../../images/ph_29_vignette.jpg'
+import image1 from '../../../images/ph_29_vignette.jpg';
+import image2 from '../../../images/light_bubbles.png';
 import '../../../stylesheets/parallax.css';
 
 function ParallaxHome() {
   return (
     <div className="parallax-container">
       <Parallax speed={1}>
-        <img src={image} className="par par-1" />
+        <img src={image1} className="par par-1" />
+        <img src={image2} className="par par-2" />
         <Parallax speed={-1.5} className="par par-text">The Crew</Parallax>
       </Parallax>
     </div>
