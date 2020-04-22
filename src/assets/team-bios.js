fa39b0db78bcf4a72e764f5334ab89fb2c062315
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-21 17:29:59 -0700

    create assets folder and store for team bios

---
+++ src/assets/team-bios.js
@@ -0,0 +1,27 @@
+export const team = {
+  kumo: {
+    urls: {},
+    bio: "",
+    image: ''
+  },
+  sid: {
+    urls: {},
+    bio: "",
+    image: ''
+  },
+  javon: {
+    urls: {},
+    bio: "",
+    image: ''
+  },
+  jake: {
+    urls: {},
+    bio: "",
+    image: ''
+  },
+  robert: {
+    urls: {},
+    bio: "",
+    image: ''
+  }
+}
