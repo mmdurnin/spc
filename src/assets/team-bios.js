8e834644c6b8ac2725b8664d390c97bc8d0b52f3
Maureen Durnin <maureenmdurnin@gmail.com>
2020-04-21 20:15:55 -0700

    add ipsem lorem placeholder to team bios

--- src/assets/team-bios.js
+++ src/assets/team-bios.js
@@ -1,27 +1,32 @@
 export const team = {
-  kumo: {
-    urls: {},
-    bio: "",
-    image: 'ph_28.jpg'
-  },
-  sid: {
-    urls: {},
-    bio: "",
-    image: 'ph_18.jpeg'
-  },
-  javon: {
-    urls: {},
-    bio: "",
-    image: 'ph_33_javon.jpg'
-  },
-  jake: {
-    urls: {},
-    bio: "",
-    image: 'ph_23_Jake_edited.jpg'
-  },
-  robert: {
-    urls: {},
-    bio: "",
-    image: 'ph_26_robert.jpg'
-  }
-}
+         kumo: {
+           urls: {},
+           bio:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+           image: "ph_28.jpg",
+         },
+         sid: {
+           urls: {},
+           bio:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+           image: "ph_18.jpeg",
+         },
+         javon: {
+           urls: {},
+           bio:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+           image: "ph_33_javon.jpg",
+         },
+         jake: {
+           urls: {},
+           bio:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+           image: "ph_23_Jake_edited.jpg",
+         },
+         robert: {
+           urls: {},
+           bio:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+           image: "ph_26_robert.jpg",
+         },
+       };
